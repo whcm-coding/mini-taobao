798604ee8d5067549c0f770d4a6c53a5231185a6
whcm-coding <472730274@qq.com>
2019-08-21 13:23:08 +0800

    chore: add internal apis

--- items/urls.py
+++ items/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = 'items'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('internal/import-all', views._importData, name='internal-add'),
+    path('internal/del-all', views._delData, name='internal-del')
 ]
--- items/views.py
+++ items/views.py
@@ -3,7 +3,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # from data import
 from .models import ItemList
+from data._itemList import context
 
 
 def index(request):
     return render(request, 'items/list.html', {'list': ItemList.objects.all()})
+
+
+def _importData(request):
+    for item in context:
+        item_temp = ItemList(
+            price=item['price'],
+            postal=item['postal'],
+            title=item['title'],
+            shopNick=item['shopNick'],
+            payNum=item['payNum'],
+            count=item['count'],
+            image=item['image'],
+        )
+        item_temp.save()
+    return HttpResponse('load data success')
+
+
+def _delData(request):
+    ItemList.objects.all().delete()
+    return HttpResponse('delete data success')
