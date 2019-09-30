5f18ccad7a638dd9c75a948016c9258fca2d59ff
whcm-coding <472730274@qq.com>
2019-09-30 16:19:01 +0800

    update sql

---
+++ data/tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+clothes = [
+    '女装',
+    "真丝小衫女洋气2019年夏季新款韩版宽松",
+    "印花雪纺衫女2019春秋新款",
+    "小香风针织开衫2018秋季新款针织衫外套"
+]
+# print(clothes)
+
+
+def print_list(the_list):
+    for each_item in the_list:
+        print(each_item)
+
+
+phones = ['手机', 'HUAWEI HUAWEI P30 PRO',
+          'Apple/苹果 iPhone 8 Plus', 'Xiaomi/小米 小米9']
+
+
+print_list(clothes)
+print_list(phones)
