72ce96c7c9a8e8125927725ad48d03fd0e481d24
whcm-coding <472730274@qq.com>
2019-09-30 20:45:28 +0800

    add verbose name and help text

--- items/models.py
+++ items/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class ItemList(models.Model):
-    price = models.CharField(max_length=200)
-    postal = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    shopNick = models.CharField(max_length=200)
-    payNum = models.CharField(max_length=200)
-    count = models.PositiveIntegerField()
-    image = models.CharField(max_length=200)
+    price = models.CharField(
+        max_length=200, verbose_name="价格", help_text="输入数字，例如：168")
+    postal = models.CharField(
+        max_length=200, verbose_name="包邮", help_text="输入True 或 False")
+    title = models.CharField(
+        max_length=200, verbose_name="服装标题", help_text="例如：真丝小衫女")
+    shopNick = models.CharField(
+        max_length=200, verbose_name="店铺名", help_text="Lee官方旗舰店")
+    payNum = models.CharField(
+        max_length=200, verbose_name="付款人数", help_text="例如：6人付款")
+    count = models.PositiveIntegerField(verbose_name="库存", help_text="例如：10")
+    image = models.CharField(max_length=200, verbose_name="图片链接",
+                             help_text="https://img.alicdn.com/imgextra/i1/123814384/O1CN01u2VMux1iFutwj2tl4_!!0-saturn_solar.jpg_220x220.jpg_.webp")
